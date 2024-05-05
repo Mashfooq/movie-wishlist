c6ed30e4059c01cf2b210fcf0b72ae0c28ff677a
Mashfooq <mashhussmashfoo@gmail.com>
2024-05-05 16:44:46 +0530

    Database connection.

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -1,8 +1,14 @@
 import dotenv from "dotenv"
 import { app } from "./app.js"
+import { client } from "./database.js"
 
 dotenv.config({
     path: './.env'
 })
 
-app.listen(process.env.PORT ?? 3002, () => console.log("Server started"))
+// Connect to the database
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err));
+
+app.listen(process.env.PORT ?? 3002, () => console.log('Server started'));
